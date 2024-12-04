0042bf5b299f8459c35ab2fad72bd26f4cc7a6f6
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-04 17:06:47 -0300

    feat:Criando um laço de repetição utilizando while
    Adicionado um laço while para iterar sobre o vetor e imprimir seus elementos.

---
+++ src/lacoRepeticaoWhile.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class lacoRepeticaoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5]; //tamanho da matriz
+    }
+}
