e580181c69ac563aff44eb7f83e2b603a1302bff
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-04 17:33:45 -0300

    feat: implementar laço while para ler valores do usuário
    Adicionado um laço while para iterar sobre o vetor e permitir que o usuário
    insira os valores nas posições do vetor usando o Scanner.

--- src/lacoRepeticaoWhile.java
+++ src/lacoRepeticaoWhile.java
@@ -4,5 +4,19 @@ public class lacoRepeticaoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[5]; //tamanho da matriz
+        System.out.println("Digite 5 números: ");
+        int index = 0; // Inicializa o índice antes do laço
+
+        while (index < numeros.length) { // Condição do laço
+            System.out.println("Número da posição " + index + ":");
+            numeros[index] = scanner.nextInt(); // Aguarda o usuário digitar
+            index++; // Incrementa o índice manualmente
+        }
+
+
+
+
+
+
     }
 }
