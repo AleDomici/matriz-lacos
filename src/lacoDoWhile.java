2d6555f59323074e5380952040f631046b5ea9ed
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-05 12:15:09 -0300

    feat: adicionando laço do-while
    primerio executa e depois avalia

--- src/lacoDoWhile.java
+++ src/lacoDoWhile.java
@@ -6,5 +6,21 @@ public class lacoDoWhile {
         int[] numeros = new int[5]; //tamanho da matriz
         System.out.println("Digite 5 números: ");
         int index = 0; // Inicializa o índice antes do laço
+
+        //primerio executa e depois avalia
+        do {
+            System.out.println("Número da posição " + index + ":");
+            numeros[index] = scanner.nextInt(); // Aguarda o usuário digitar
+            index++;
+        }  while (index < numeros.length);
+
+        System.out.println("Números digitados: ");
+        index = 0;
+
+        while (index < numeros.length) {
+            System.out.println("Número da posição " + index + ": " + numeros[index]);
+            index++;
+        }
+        scanner.close();
     }
 }
