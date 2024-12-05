4af6d7a1c448cbbfeb6890b12316bf26628780c9
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-05 11:40:55 -0300

    feat: Declaração de variáveis e inicialização

---
+++ src/lacoDoWhile.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class lacoDoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5]; //tamanho da matriz
+        System.out.println("Digite 5 números: ");
+        int index = 0; // Inicializa o índice antes do laço
+    }
+}
