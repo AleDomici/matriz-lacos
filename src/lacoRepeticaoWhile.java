dfd8cdbabae2f65aba7eda5dd057134eee16f66d
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-05 10:31:08 -0300

    feat: implementa laço while para exibição de valores digitados pelo usuário
    Adicionado um laço while para exibir valores digitados pelo usuario

--- src/lacoRepeticaoWhile.java
+++ src/lacoRepeticaoWhile.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
+                                //Declaração de variáveis e inicialização
 public class lacoRepeticaoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -7,13 +8,21 @@ public class lacoRepeticaoWhile {
         System.out.println("Digite 5 números: ");
         int index = 0; // Inicializa o índice antes do laço
 
+                                //Primeiro laço while - Coleta de números
         while (index < numeros.length) { // Condição do laço
             System.out.println("Número da posição " + index + ":");
             numeros[index] = scanner.nextInt(); // Aguarda o usuário digitar
             index++; // Incrementa o índice manualmente
         }
+                                     //Exibição dos números digitados
+        System.out.println("Números digitados: ");
+        index = 0;
 
-
+        while (index < numeros.length) {
+            System.out.println("Número da posição " + index + ": " + numeros[index]);
+            index++;
+        }
+        scanner.close();
 
 
 
